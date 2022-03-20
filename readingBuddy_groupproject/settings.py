722010af4990738413299abf38519f2ad7d34504
Hannah Hughes <2712406h@student.gla.ac.uk>
2022-03-20 21:40:51 +0000

    Re pushing overwritten changes to userpage

--- readingBuddy_groupproject/settings.py
+++ readingBuddy_groupproject/settings.py
@@ -124,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/images/'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
